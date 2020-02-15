7f872735f2d056d8d9469a6d40743ce8e62fb39a
Yafim Vainilovich <yafimsva@gmail.com>
2020-02-15 12:19:22 -0800

    Updates

---
+++ precache-manifest.b852a49ee9bc285fe1afd4b041224a93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f288001d24e7d253b164ba4f58b4df48",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4ee8fc30be6e71a3188e",
+    "url": "/monster-rolodex/static/css/main.b926d4d4.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ee8fc30be6e71a3188e",
+    "url": "/monster-rolodex/static/js/main.79733caf.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
